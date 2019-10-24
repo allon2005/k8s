ee11921993a96a1b323e32ee08deb7d0292ffc05
root <root@ip-10-0-6-215.ec2.internal>
2019-10-24 23:07:06 +0000

    change

--- kubeadm_reset.sh
+++ kubeadm_reset.sh
@@ -10,8 +10,7 @@ iptables -F && iptables -t nat -F && iptables -t mangle -F && iptables -X
 
 rm $HOME/.kube/config
 
-Have1025=netstat -tulpn | grep 1025
+netstat -tulpn | grep 1025
 
-echo "check 1025* port: $Have1025"
 
 kubectl cluster-info
